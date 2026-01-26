685e51d682d04b7807e1cbb9337ef6404f4cc2fb
adigitX <adityakammati.workspace@gmail.com>
2026-01-27 00:16:20 +0530

    Add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Header } from './Header';
+export { Sidebar } from './Sidebar';
+export { Hero } from './Hero';
+export { ColorGrid } from './ColorGrid';
+export { DownloadModal } from './DownloadModal';
+export { ColorPicker } from './ColorPicker';
