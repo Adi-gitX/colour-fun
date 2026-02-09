957de142a8a5da8b3208485eb86de317926f48f6
adigitX <adityakammati.workspace@gmail.com>
2026-02-09 09:11:25 +0530

    Clean up project structure - move app files to app/ folder

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Header } from './Header';
-export { Sidebar } from './Sidebar';
-export { Hero } from './Hero';
-export { ColorGrid } from './ColorGrid';
-export { DownloadModal } from './DownloadModal';
-export { ColorPicker } from './ColorPicker';
